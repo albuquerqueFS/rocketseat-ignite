ea0ce74a59773d21082237bf7dc15347324aac20
Henrique Albuquerque <henrique_barretto@outlook.com>
2022-06-23 01:04:05 -0300

    config: react refresh

--- 01-github-explorer/src/components/RepositoryItem.jsx
+++ 01-github-explorer/src/components/RepositoryItem.jsx
@@ -2,7 +2,7 @@ export function RepositoryItem({ repository }) {
   return (
     <li>
       <strong>{repository?.name ?? "Default"}</strong>
-      <p>Forms in React</p>
+      <p>Forms in React </p>
       <a href="">Acessar repositório</a>
     </li>
   );
